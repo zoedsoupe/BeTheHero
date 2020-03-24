a00f693dc1860fcbdfda6f97132de5ef4b76a183
Zoey <zoey.spessanha@zeetech.io>
2020-03-24 12:14:41 -0300

    sessionsController and route, cors module

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -2,7 +2,26 @@ const connection = require("../database/connection");
 
 module.exports = {
   async index(Request, Response) {
-    const incidents = await connection("incidents").select("*");
+    const { page = 1 } = Request.query;
+
+    const [count] = await connection("incidents").count();
+
+    console.log(count);
+
+    const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.wpp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
+
+    Response.header("X-Total-Count", count["count(*)"]);
 
     return Response.json(incidents);
   },
