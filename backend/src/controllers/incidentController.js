66b928946c0fad980fca1056865fbdcfe18ee576
Zoey <zoey.spessanha@zeetech.io>
2020-03-24 11:24:40 -0300

    incidentController

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -1,7 +1,23 @@
 const connection = require("../database/connection");
 
 module.exports = {
+  async index(Request, Response) {
+    const incidents = await connection("incidents").select("*");
+
+    return Response.json(incidents);
+  },
   async create(Request, Response) {
     const { title, description, value } = Request.body;
+
+    const ong_id = Request.headers.authorization;
+
+    const [id] = await connection("incidents").insert({
+      title,
+      description,
+      value,
+      ong_id
+    });
+
+    return Response.json({ id });
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const ongController = require("./controllers/ongController");
+const incidentController = require("./controllers/incidentController");
 
 const routes = express.Router();
 
 routes.get("/ongs", ongController.index);
-
 routes.post("/ongs", ongController.create);
+
+routes.get("/incidents", incidentController.index);
+routes.post("/incidents", incidentController.create);
 
 module.exports = routes;
