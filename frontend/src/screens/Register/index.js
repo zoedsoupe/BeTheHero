412267bcecdb7aa2d82ea3cc851b7b50441e64fc
Matheus <matheus_pessanha2001@outlook.com>
2020-03-25 11:20:55 -0300

    route Register

---
+++ frontend/src/screens/Register/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./styles.css";
+import logoPic from "../../assets/logo.svg";
+
+export default function Register() {
+  return (
+      <div className="register-container">
+          <div className="content">
+              <section>
+
+              </section>
+              <form>
+
+              </form>
+          </div>
+      </div>
+  );
+}
