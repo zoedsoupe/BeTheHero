f23201b6fe9b82f627bd5890fe0992f60eae5cff
Zoey <zoey.spessanha@zeetech.io>
2020-03-25 11:38:21 -0300

    register screen

--- frontend/src/screens/Register/index.js
+++ frontend/src/screens/Register/index.js
@@ -1,19 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 
 import "./styles.css";
 import logoPic from "../../assets/logo.svg";
 
 export default function Register() {
   return (
-      <div className="register-container">
-          <div className="content">
-              <section>
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoPic} alt="Be The Hero" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastrp, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
 
-              </section>
-              <form>
-
-              </form>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" /> Não tenho cadastro
+          </Link>
+        </section>
+        <form>
+          <input placeholder="Nome da ONG" />
+          <input type="email" placeholder="Email" />
+          <input placeholder="Whatsapp" />
+          <div className="input-group">
+            <input placeholder="Cidade" />
+            <input placeholder="UF" style={{ width: 80 }} />
           </div>
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
       </div>
+    </div>
   );
 }
