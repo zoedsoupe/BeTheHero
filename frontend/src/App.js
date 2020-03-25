20ab27265807b096375b3a7c7ff755a15f099d85
Matheus <matheus_pessanha2001@outlook.com>
2020-03-25 09:44:40 -0300

    state and immutability on React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello Omnistack</h1>;
+  const [i, setI] = useState(0);
+
+  //Array [valor, value update function]
+
+  function increment() {
+    setI(i + 1);
+  }
+  return (
+    <div>
+      <Header>Contador: {i}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
 }
 
 export default App;
