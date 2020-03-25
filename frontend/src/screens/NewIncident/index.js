567369715a4562bd2fdb46ca97a1e1754fb97495
Matheus <matheus_pessanha2001@outlook.com>
2020-03-25 12:37:49 -0300

    NewIncident Route

---
+++ frontend/src/screens/NewIncident/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
+
+import "./styles.css";
+import logoPic from "../../assets/logo.svg";
+
+export default function NewIncident() {
+    return <h1>New Incident</h1>
+}
