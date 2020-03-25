4be2b3512754b21316370e5fdf1ccbce2512dab4
Matheus <matheus_pessanha2001@outlook.com>
2020-03-25 10:19:15 -0300

    basic form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Logon from "./screens/Logon";
+import "./global.css";
 
 function App() {
   return <Logon />;
--- frontend/src/screens/Logon/index.js
+++ frontend/src/screens/Logon/index.js
@@ -1,6 +1,24 @@
 import React from "react";
+
 import "./styles.css";
+import logoPic from "../../assets/logo.svg";
+import heroesPic from "../../assets/heroes.png";
 
 export default function Logon() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoPic} alt="Be The Hero" />
+
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+
+          <a href="/register">Não tenho cadastro</a>
+        </form>
+      </section>
+      <img src={heroesPic} alt="Heroes" />
+    </div>
+  );
 }
