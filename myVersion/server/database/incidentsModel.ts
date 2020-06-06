1fcf144bd45fd0ebe71a55b654360ba3bebd9744
Matheus <matheus_pessanha2001@outlook.com>
2020-06-06 12:31:15 -0300

    completed incidentsController

--- myVersion/server/database/incidentsModel.ts
+++ myVersion/server/database/incidentsModel.ts
@@ -4,12 +4,14 @@ interface Incident extends Document {
   title: String;
   description: String;
   value: number;
+  ongId: string;
 }
 
 const incidentsSchema = new Schema({
   title: String,
   description: String,
   value: Number,
+  ongId: String,
 });
 
 const Incidents = mongoose.model<Incident>("Incidents", incidentsSchema);
