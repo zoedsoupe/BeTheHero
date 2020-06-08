e0dc63bdcca142e38dd8c0ef5d6f40ede9f215d8
Zoey <zoey.spessanha@zeetech.io>
2020-06-07 21:16:15 -0300

    backend deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,4 +25,6 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(Number(`${process.env.PORT}`), process.env.IP!, () => console.log("Server started!!!"));
+app.listen(Number(`${process.env.PORT}`), () =>
+  console.log("Server started!!!")
+);
